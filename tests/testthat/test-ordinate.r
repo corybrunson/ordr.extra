0148e68a42264bc998788b4d756c6e0e0dde0c6d
Jason Cory Brunson <jason.brunson@medicine.ufl.edu>
2022-07-21 11:52:40 -0400

    incorporate ca ordinate test

--- tests/testthat/test-ordinate.r
+++ tests/testthat/test-ordinate.r
@@ -6,6 +6,7 @@ rdata <- data.frame(
 )
 
 test_that("`ordinate()` can handle appropriate models", {
+  expect_error(ordinate(rdata, everything(), ca::ca), regexp = NA)
   expect_error(ordinate(rdata, everything(), ade4::nipals), regexp = NA)
   expect_error(ordinate(rdata, everything(), nipals_ord), regexp = NA)
 })
