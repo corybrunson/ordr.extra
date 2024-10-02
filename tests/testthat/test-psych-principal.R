3d1c56313a260e639d957cab49052c366a20e1bd
JB Gracey <jbgracey6@gmail.com>
2024-10-02 15:32:13 -0400

    Revised methods, example, and tests for pull request

---
+++ tests/testthat/test-psych-principal.R
@@ -0,0 +1,21 @@
+fit_principal <- principal(iris[, -5], nfactors = 4, rotate = "none")
+
+test_that("'principal' accessors have consistent dimensions", {
+  expect_equal(ncol(get_rows(fit_principal)), ncol(get_cols(fit_principal)))
+  expect_equal(ncol(get_rows(fit_principal)),
+               length(recover_inertia(fit_principal)))
+})
+
+test_that("'principal' has specified distribution of inertia", {
+  expect_type(recover_conference(fit_principal), "double")
+  expect_vector(recover_conference(fit_principal), size = 2L)
+})
+
+test_that("'principal' augmentations are consistent with '.element' column", {
+  expect_equal(".element" %in% names(recover_aug_rows(fit_principal)),
+               ".element" %in% names(recover_aug_cols(fit_principal)))
+})
+
+test_that("`as_tbl_ord()` coerces 'principal' objects", {
+  expect_true(valid_tbl_ord(as_tbl_ord(fit_principal)))
+})
