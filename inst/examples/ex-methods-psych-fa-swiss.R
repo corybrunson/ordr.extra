32e1269ae419a3af369f3711006ce83a9057243a
JB Gracey <jbgracey6@gmail.com>
2025-03-23 14:40:24 -0400

    Added sections for biplot and inv_weight

--- inst/examples/ex-methods-psych-fa-swiss.R
+++ inst/examples/ex-methods-psych-fa-swiss.R
@@ -10,8 +10,17 @@ swiss_fa <- psych::fa(r = swiss, nfactors = 2L, rotate = "varimax", scores = "re
 
 # recover loadings
 get_cols(swiss_fa, elements = "active")
+# recover pseudoinverse of weights
+get_cols(swiss_fa, elements = "inv_weight")
 # recover scores
 head(get_rows(swiss_fa, elements = "score"))
 
 # augment column loadings with uniquenesses
 (swiss_fa <- augment_ord(swiss_fa))
+
+# symmetric biplot
+swiss_fa %>%
+  ggbiplot() +
+  theme_bw() +
+  geom_cols_vector(elements = "inv_weight") + 
+  expand_limits(x = c(-2, 2.5), y = c(-1.5, 2))
