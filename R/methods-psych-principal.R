c1879e38a7e9d6f6d269af2bd8a5781d6c2c2acc
JB Gracey <jbgracey6@gmail.com>
2025-03-31 10:17:39 -0400

    Moved 'name' definition into if-statement on line 76

--- R/methods-psych-principal.R
+++ R/methods-psych-principal.R
@@ -72,8 +72,8 @@ recover_aug_rows.principal <- function(x) {
   res <- tibble(.rows = 0L)
   
   # scores as supplementary points
-  name <- rownames(x[["scores"]])
   if (!is.null(x[["scores"]])) {
+    name <- rownames(x[["scores"]])
     res_sup <- if (is.null(name)) {
       tibble(.rows = nrow(x[["scores"]]))
     } else {
