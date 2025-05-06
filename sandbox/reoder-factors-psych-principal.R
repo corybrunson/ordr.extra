f920b061229df42b5ca68d0a988627c333e0a941
JB Gracey <jbgracey6@gmail.com>
2025-05-06 08:30:23 -0400

    Added notes

--- sandbox/reoder-factors-psych-principal.R
+++ sandbox/reoder-factors-psych-principal.R
@@ -3,9 +3,18 @@ library(ordr.extra)
 library(psych)
 library(tibble)
 
+# create PCA object with healthcare worker data
 hcw_pca <- ordinate(hcw[36:49], ~principal(., nfactors = 14, rotate = "none"))
 
+# generate blank scree plot to see order of PCs on x-axis
 tidy(hcw_pca) |>
   ggplot(aes(x = name,y = inertia))
 
-recover_coord(hcw_pca)
+# inspect output of recover_aug_coord
+recover_aug_coord(hcw_pca)
+
+# The ordering of the "name" column in `recover_aug_coord(hcw_pca)` is what we
+# want -- ordered by the number following "PC". However, this was the case when
+# printing the output before my changes, too. The biplot axis is still wrong, 
+# though. I tried reading the documentation and some source code for ggplot, but
+# couldn't come up with anything there.
