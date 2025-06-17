3f5da1b4a446b924570520eb6aa89dcbd5c7191b
JB Gracey <jbgracey6@gmail.com>
2025-06-17 12:02:18 -0400

    Fixed PCA biplot to show scores and created FA biplot to try showing inverse weights

--- sandbox/iris-fa-biplot-with-inv_weight.R
+++ sandbox/iris-fa-biplot-with-inv_weight.R
@@ -1,13 +1,13 @@
 # load psych package
 library(psych)
 
-# pca on iris
+# fa on iris
 iris[1:4] |>
-  ordinate(~principal(., nfactors = 2)) |>
-  print() -> iris_pca
+  ordinate(~fa(., nfactors = 2)) |>
+  print() -> iris_fa
 
 # produce biplot with scores plotted
 
-ggbiplot(iris_pca, scale_cols = 3) +
+ggbiplot(iris_fa, scale_cols = 3) +
   geom_cols_vector(color = "red") +
   geom_rows_point(elements = "score")
