b9d8cbcd38b7c6122a42471988fc583b27336e3b
JB Gracey <jbgracey6@gmail.com>
2026-04-15 12:30:08 -0400

    Add test for (lack of) conference of inertia

--- tests/testthat/test-lpca.r
+++ tests/testthat/test-lpca.r
@@ -20,6 +20,12 @@ test_that("'logisticPCA' accessors have consistent dimensions", {
   expect_equal(ncol(get_rows(fit_clpca)), ncol(get_cols(fit_clpca)))
 })
 
+test_that("'logisticPCA' has specified distribution of inertia", {
+  expect_equal(recover_inertia(fit_lsvd), as.double(NA))
+  expect_equal(recover_inertia(fit_lpca), as.double(NA))
+  expect_equal(recover_inertia(fit_clpca), as.double(NA))
+})
+
 test_that("`as_tbl_ord()` coerces 'lsvd', 'lpca', & 'clpca' objects", {
   expect_true(valid_tbl_ord(as_tbl_ord(fit_lsvd)))
   expect_true(valid_tbl_ord(as_tbl_ord(fit_lpca)))
@@ -29,4 +35,8 @@ test_that("`as_tbl_ord()` coerces 'lsvd', 'lpca', & 'clpca' objects", {
 test_that("'lsvd' augmentations are consistent with '.element' column", {
   expect_equal(".element" %in% names(recover_aug_rows(fit_lsvd)),
                ".element" %in% names(recover_aug_cols(fit_lsvd)))
+  expect_equal(".element" %in% names(recover_aug_rows(fit_lpca)),
+               ".element" %in% names(recover_aug_cols(fit_lpca)))
+  expect_equal(".element" %in% names(recover_aug_rows(fit_clpca)),
+               ".element" %in% names(recover_aug_cols(fit_clpca)))
 })
